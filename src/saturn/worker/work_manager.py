01205c0adc86ff5517d0393965ed0e4373628e6f
isra17 <isra017@gmail.com>
2021-10-08 15:26:34 -0400

    Worker: Basic implementation of the work manager

--- src/saturn/worker/work_manager.py
+++ src/saturn/worker/work_manager.py
@@ -2,6 +2,8 @@ import dataclasses
 from typing import Optional
 
 from saturn.client.worker_manager import WorkerManagerClient
+from saturn.utils import flatten
+from saturn.utils import tri_split
 
 from .queue import Queue
 
@@ -9,13 +11,31 @@ from .queue import Queue
 @dataclasses.dataclass
 class QueuesSync:
     add: list[Queue]
-    keep: list[Queue]
     drop: list[Queue]
 
 
+ItemsQueues = dict[str, list[Queue]]
+
+
 class WorkManager:
+    client: WorkerManagerClient
+    work_items_queues: ItemsQueues
+
     def __init__(self, client: Optional[WorkerManagerClient] = None) -> None:
-        self.client = client
+        self.client = client or WorkerManagerClient()
+        self.work_items_queues = {}
 
     async def sync_queues(self) -> QueuesSync:
-        return QueuesSync([], [], [])
+        sync_response = await self.client.sync()
+
+        current_items = set(self.work_items_queues.keys())
+        sync_items = {item.id for item in sync_response.items}
+        add, _, drop = tri_split(sync_items, current_items)
+
+        added_items_queues: ItemsQueues = {i: [Queue()] for i in add}
+        self.work_items_queues.update(added_items_queues)
+        add_queues = list(flatten(added_items_queues.values()))
+
+        drop_queues = list(flatten(self.work_items_queues.pop(k) for k in drop))
+
+        return QueuesSync(add_queues, drop_queues)
