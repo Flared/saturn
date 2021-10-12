2695986869876a0f3a68cfffeaf06db732f3ddd0
aviau <alexandre@alexandreviau.net>
2021-10-12 14:56:06 -0400

    pytest: install icdiff

--- noxfile.py
+++ noxfile.py
@@ -46,7 +46,7 @@ def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> Non
 def tests(session: Session) -> None:
     args = session.posargs
     install_project(session)
-    install_with_constraints(session, "pytest", "pytest-asyncio")
+    install_with_constraints(session, "pytest", "pytest-asyncio", "pytest-icdiff")
     session.run("pytest", *args)
 
 
