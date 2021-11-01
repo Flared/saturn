ab8b6e9dabd3a464978d98a8b04c808350dcbeac
aviau <alexandre@alexandreviau.net>
2021-11-01 14:18:29 -0400

    worker: load config from venv

--- src/saturn_engine/worker/services/config.py
+++ src/saturn_engine/worker/services/config.py
@@ -1,6 +1,30 @@
-from argparse import Namespace
+import dataclasses
+import os
+from functools import cached_property
+
+
+@dataclasses.dataclass
+class RabbitMQConfig:
+    url: str
+
+
+@dataclasses.dataclass
+class WorkerManagerConfig:
+    url: str
 
 
 class ConfigService:
     def __init__(self) -> None:
-        self.rabbitmq = Namespace(url="amqp://127.0.0.1/")
+        pass
+
+    @cached_property
+    def rabbitmq(self) -> RabbitMQConfig:
+        return RabbitMQConfig(
+            url=os.environ.get("SATURN_AMQP_URL", "amqp://127.0.0.1/"),
+        )
+
+    @cached_property
+    def worker_manager(self) -> WorkerManagerConfig:
+        return WorkerManagerConfig(
+            url=os.environ.get("SATURN_WORKER_MANAGER_URL", "http://localhost:5000"),
+        )
