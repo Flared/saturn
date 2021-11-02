01b2c38b2bcfbc00a6e4ba97d0a7bbe7e7fb0e1a
Alexandre Viau <alexandre@alexandreviau.net>
2021-11-02 18:06:44 -0400

    declarative: split in three files (#42)

--- src/saturn_engine/worker_manager/config/declarative.py
+++ src/saturn_engine/worker_manager/config/declarative.py
@@ -1,49 +1,10 @@
 import dataclasses
-import functools
 import os
-from typing import Any
 from typing import Optional
 
-import desert
-import marshmallow
 import yaml
 
-from saturn_engine.core.api import InventoryItem
-
-
-@dataclasses.dataclass
-class ObjectMetadata:
-    name: str
-
-
-@dataclasses.dataclass
-class BaseObject:
-    metadata: ObjectMetadata
-    apiVersion: str
-    kind: str
-
-
-@dataclasses.dataclass
-class InventorySpec:
-    type: str
-    options: dict[str, Any]
-
-
-@dataclasses.dataclass
-class Inventory(BaseObject):
-    spec: InventorySpec
-
-    @classmethod
-    @functools.cache
-    def schema(cls) -> marshmallow.Schema:
-        return desert.schema(cls)
-
-    def to_core_object(self) -> InventoryItem:
-        return InventoryItem(
-            name=self.metadata.name,
-            type=self.spec.type,
-            options=self.spec.options,
-        )
+from .declarative_inventory import Inventory
 
 
 @dataclasses.dataclass
