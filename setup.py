56829f233389cc0eb4564cee7012c0286b8e75ce
isra17 <isra017@gmail.com>
2021-11-11 17:32:38 -0500

    dev: make project locally installable with pip

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="saturn_engine",
+    version="0.3dev",
+    packages=["saturn_engine"],
+    package_dir={"":"src"},
+    install_requires=[],
+)
