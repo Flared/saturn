c08145c7a3e49aef1b07a6f941cef3763ad06822
isra17 <isra017@gmail.com>
2021-12-16 17:41:56 -0500

    saturn(hooks): ContextHook implementation

--- src/saturn_engine/worker/services/hooks.py
+++ src/saturn_engine/worker/services/hooks.py
@@ -1,15 +1,22 @@
+from collections.abc import Generator
 from typing import Any
 from typing import Awaitable
 from typing import Callable
 from typing import Generic
+from typing import Optional
 from typing import TypeVar
+from typing import Union
 
 TCallable = TypeVar("TCallable", bound=Callable[..., Any])
 TAwaitable = TypeVar("TAwaitable", bound=Callable[..., Awaitable])
 
+A = TypeVar("A")
+R = TypeVar("R")
+GGenerators = list[Generator[None, A, None]]
+
 
 class EventHook(Generic[TCallable]):
-    def __init__(self, *, error_handler: Callable[[Exception], None]):
+    def __init__(self, *, error_handler: Optional[Callable[[Exception], None]] = None):
         self.error_handler = error_handler
         self.handlers: list[TCallable] = []
 
@@ -52,3 +59,90 @@ class AsyncEventHook(Generic[TAwaitable]):
             except Exception as e:
                 if self.error_handler:
                     self.error_handler(e)
+
+
+class ContextHook(Generic[A, R]):
+    def __init__(self, *, error_handler: Optional[Callable[[Exception], None]] = None):
+        self.error_handler = error_handler
+        self.handlers: list[
+            Union[Callable[[A], None], Callable[[A], Generator[None, R, None]]]
+        ] = []
+
+    def register(
+        self,
+        handler: Union[Callable[[A], None], Callable[[A], Generator[None, R, None]]],
+    ) -> Union[Callable[[A], None], Callable[[A], Generator[None, R, None]]]:
+        self.handlers.append(handler)
+        return handler
+
+    def emit(self, scope: Callable[[A], R]) -> "ContextHookEmiter[A, R]":
+        return ContextHookEmiter(self, scope)
+
+
+class ContextHookEmiter(Generic[A, R]):
+    def __init__(self, hook: ContextHook[A, R], scope: Callable[[A], R]):
+        self.hook = hook
+        self.scope = scope
+
+    def __call__(self, arg: A) -> R:
+        generators = self.on_call(arg)
+        try:
+            result = self.scope(arg)
+            self.on_result(generators, result)
+        except Exception as e:
+            self.on_error(generators, e)
+            raise
+        return result
+
+    def on_call(self, arg: A) -> GGenerators:
+        # Call handlers and collect generators
+        generators = []
+        for handler in self.hook.handlers:
+            try:
+                result = handler(arg)
+                if isinstance(result, Generator):
+                    next(result)
+                    generators.append(result)
+            except Exception as e:
+                self.handle_error(e)
+        return generators
+
+    def on_error(self, generators: GGenerators, error: Exception) -> None:
+        # If `scope` raise, propagate the error to all handlers.
+        for generator in reversed(generators):
+            try:
+                generator.throw(error)
+                # Ensure the generator is closed.
+                generator.close()
+                try:
+                    raise ValueError("Handler must yield once")
+                except Exception as e:
+                    self.handle_error(e)
+            except StopIteration:
+                # The generator could be done.
+                pass
+            except Exception as gen_e:
+                # Ensure the error raised is not the one that was passed.
+                if gen_e is not error:
+                    self.handle_error(gen_e)
+
+    def on_result(self, generators: GGenerators, result: R) -> None:
+        # Call handlers in reverse order with the result
+        for generator in reversed(generators):
+            try:
+                generator.send(result)
+                # Ensure the generator is closed.
+                generator.close()
+                try:
+                    raise ValueError("Handler must yield once")
+                except Exception as e:
+                    self.handle_error(e)
+            except StopIteration:
+                # This is expected.
+                pass
+            except Exception as e:
+                self.handle_error(e)
+
+    def handle_error(self, error: Exception) -> None:
+        if self.hook.error_handler:
+            self.hook.error_handler(error)
--- tests/worker/services/test_hooks.py
+++ tests/worker/services/test_hooks.py
@@ -1,9 +1,11 @@
+from collections.abc import Generator
 from typing import Protocol
 from unittest import mock
 
 import pytest
 
 from saturn_engine.worker.services.hooks import AsyncEventHook
+from saturn_engine.worker.services.hooks import ContextHook
 from saturn_engine.worker.services.hooks import EventHook
 
 
@@ -66,3 +68,193 @@ async def test_async_event_hook() -> None:
     await hook.emit("d", b="e")
     error_handler.assert_called_once_with(error)
     handler2.assert_awaited_once_with("d", b="e")
+
+
+def test_context_hook() -> None:
+    error_handler = mock.Mock()
+    m = mock.Mock()
+    hook: ContextHook[str, str] = ContextHook(error_handler=error_handler)
+
+    @hook.register
+    def handler(a: str) -> Generator[None, str, None]:
+        m(handler, "before")
+        try:
+            r = yield
+            m(handler, "after", r)
+        except Exception as e:
+            m(handler, "error", e)
+
+    @hook.register
+    def handler_before(a: str) -> None:
+        m(handler_before, "before")
+
+    @hook.register
+    def handler_after(a: str) -> Generator[None, str, None]:
+        m(handler_after, "before")
+        yield
+        m(handler_after, "after")
+
+    @hook.emit
+    def scope(a: str) -> str:
+        m("in", a)
+        return "c"
+
+    assert scope("1") == "c"
+
+    m.assert_has_calls(
+        [
+            mock.call(handler, "before"),
+            mock.call(handler_before, "before"),
+            mock.call(handler_after, "before"),
+            mock.call("in", "1"),
+            mock.call(handler_after, "after"),
+            mock.call(handler, "after", "c"),
+        ]
+    )
+
+    m.reset_mock()
+    e = ValueError()
+
+    with pytest.raises(ValueError):
+
+        @hook.emit
+        def scope_raising(a: str) -> str:
+            m("in", a)
+            raise e
+
+        scope_raising("1")
+
+    m.assert_has_calls(
+        [
+            mock.call(handler, "before"),
+            mock.call(handler_before, "before"),
+            mock.call(handler_after, "before"),
+            mock.call("in", "1"),
+            mock.call(handler, "error", e),
+        ]
+    )
+
+    error_handler.assert_not_called()
+
+
+def test_context_hook_errors() -> None:
+    error_handler = mock.Mock()
+    m = mock.Mock()
+    hook: ContextHook[None, None] = ContextHook(error_handler=error_handler)
+
+    @hook.register
+    def call_raises(_: None) -> Generator[None, None, None]:
+        m(call_raises)
+        raise ValueError
+
+    @hook.register
+    def generator_raises(_: None) -> Generator[None, None, None]:
+        m(generator_raises)
+        raise ValueError
+        yield
+
+    @hook.register
+    def generator_raises_after(_: None) -> Generator[None, None, None]:
+        yield
+        m(generator_raises_after)
+        raise ValueError
+
+    @hook.register
+    def generator_many_yield(_: None) -> Generator[None, None, None]:
+        yield
+        m(generator_many_yield)
+        yield
+        m(None)
+        raise ValueError
+
+    @hook.register
+    def generator_many_yield_wont_stop(_: None) -> Generator[None, None, None]:
+        yield
+        try:
+            yield
+            m(None)
+        except GeneratorExit:
+            m(generator_many_yield_wont_stop)
+            yield
+            m(None)
+
+    @hook.emit
+    def scope(_: None) -> None:
+        pass
+
+    scope(None)
+
+    m.assert_has_calls(
+        [
+            mock.call(call_raises),
+            mock.call(generator_raises),
+            mock.call(generator_many_yield_wont_stop),
+            mock.call(generator_many_yield),
+            mock.call(generator_raises_after),
+        ]
+    )
+
+    assert error_handler.call_count == 5
+
+
+def test_context_hook_error_errors() -> None:  # noqa: C901  # Ignore complexity errors.
+    error_handler = mock.Mock()
+    m = mock.Mock()
+    hook: ContextHook[None, None] = ContextHook(error_handler=error_handler)
+
+    @hook.register
+    def generator_raise_in_except(_: None) -> Generator[None, None, None]:
+        try:
+            yield
+        except Exception:
+            m(generator_raise_in_except)
+            raise ValueError from None
+
+    @hook.register
+    def generator_yield_in_except(_: None) -> Generator[None, None, None]:
+        try:
+            yield
+        except Exception:
+            m(generator_yield_in_except)
+            yield
+            m(None)
+
+    @hook.register
+    def generator_wont_stop(_: None) -> Generator[None, None, None]:
+        try:
+            yield
+        except Exception:
+            try:
+                yield
+            except GeneratorExit:
+                m(generator_wont_stop)
+                yield
+                m(None)
+
+    @hook.register
+    def generator_reraise(_: None) -> Generator[None, None, None]:
+        try:
+            yield
+        except Exception:
+            m(generator_reraise)
+            raise
+
+    e = ValueError()
+
+    with pytest.raises(ValueError):
+
+        @hook.emit
+        def scope(_: None) -> None:
+            raise e
+
+        scope(None)
+    m.assert_has_calls(
+        [
+            mock.call(generator_reraise),
+            mock.call(generator_wont_stop),
+            mock.call(generator_yield_in_except),
+            mock.call(generator_raise_in_except),
+        ]
+    )
+
+    assert error_handler.call_count == 3
