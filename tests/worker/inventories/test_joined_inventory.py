154020d3ed22d016d3bab78bfff704297ea439d3
isra17 <isra017@gmail.com>
2021-12-21 16:26:52 -0500

    worker(JoinedInventory): Fix bug with `after` value in sub-inventory being applied for all future items

--- tests/worker/inventories/test_joined_inventory.py
+++ tests/worker/inventories/test_joined_inventory.py
@@ -1,5 +1,6 @@
 import json
 
+import asyncstdlib as alib
 import pytest
 
 from saturn_engine.worker.inventories import JoinedInventory
@@ -25,7 +26,7 @@ async def test_joined_inventory() -> None:
         },
         services=None,
     )
-    batch = await inventory.next_batch()
+    batch = await alib.list(inventory.iterate())
     assert [(json.loads(i.id), i.args) for i in batch] == [
         ({"b": "0"}, {"a": {"n": 1}, "b": {"c": "A"}}),
         ({"b": "1"}, {"a": {"n": 1}, "b": {"c": "B"}}),
@@ -38,9 +39,11 @@ async def test_joined_inventory() -> None:
         ({"a": "1", "b": "2"}, {"a": {"n": 3}, "b": {"c": "C"}}),
     ]
 
-    batch = await inventory.next_batch(after='{"a": "1", "b": "0"}')
+    batch = await alib.list(inventory.iterate(after='{"a": "0", "b": "1"}'))
     assert [(json.loads(i.id), i.args) for i in batch] == [
+        ({"a": "0", "b": "2"}, {"a": {"n": 2}, "b": {"c": "C"}}),
+        ({"a": "1", "b": "0"}, {"a": {"n": 3}, "b": {"c": "A"}}),
         ({"a": "1", "b": "1"}, {"a": {"n": 3}, "b": {"c": "B"}}),
         ({"a": "1", "b": "2"}, {"a": {"n": 3}, "b": {"c": "C"}}),
     ]
-    assert not await inventory.next_batch(after='{"a": "1", "b": "2"}')
+    assert not await alib.list(inventory.iterate(after='{"a": "1", "b": "2"}'))
