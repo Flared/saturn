4e6ff9cbfbbc7c8cdb647ee659e3a321895605c1
Kevin Goyette <kevin.goyette@flare.systems>
2022-01-13 16:09:33 -0500

    release v0.2.0dev20

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="saturn_engine",
-    version="0.2.0dev19", # Also in `pyproject.toml`
+    version="0.2.0dev20", # Also in `pyproject.toml`
     packages=["saturn_engine"],
     package_dir={"":"src"},
     install_requires=[],
