5cf9bea5018d7ae6763f0448cb15df8a41da8d6e
isra17 <isra017@gmail.com>
2022-01-18 14:38:09 -0500

    worker(metrics): Move metrics services in their own namespace

--- src/saturn_engine/worker/services/metrics/base.py
+++ src/saturn_engine/worker/services/metrics/base.py
@@ -1,22 +1,18 @@
 from typing import Generic
 from typing import Optional
 
-import asyncio
-import contextlib
-from collections import defaultdict
 from collections.abc import AsyncGenerator
 
 from saturn_engine.core import PipelineResult
 from saturn_engine.worker.pipeline_message import PipelineMessage
 from saturn_engine.worker.services.hooks import MessagePublished
 
-from . import BaseServices
 from . import Service
 from . import TOptions
 from . import TServices
 
 
-class MetricsService(Generic[TServices, TOptions], Service[TServices, TOptions]):
+class BaseMetricsService(Generic[TServices, TOptions], Service[TServices, TOptions]):
     async def open(self) -> None:
         self.services.hooks.message_polled.register(self.on_message_polled)
         self.services.hooks.message_scheduled.register(self.on_message_scheduled)
@@ -62,34 +58,3 @@ class MetricsService(Generic[TServices, TOptions], Service[TServices, TOptions])
         self, key: str, *, count: int = 1, params: Optional[dict[str, str]] = None
     ) -> None:
         pass
-
-
-class MemoryMetrics(MetricsService[BaseServices, None]):
-    name = "memory_metrics"
-
-    async def open(self) -> None:
-        await super().open()
-        self.counters: dict[str, int] = defaultdict(int)
-        self.printer_task = asyncio.create_task(self.print_metrics())
-
-    async def close(self) -> None:
-        self.printer_task.cancel()
-        with contextlib.suppress(asyncio.CancelledError):
-            await self.printer_task
-
-    async def incr(
-        self, key: str, *, count: int = 1, params: Optional[dict[str, str]] = None
-    ) -> None:
-        self.counters[key] += count
-        if params:
-            self.counters[f"{key}{{{stringify_params(params)}}}"] += count
-
-    async def print_metrics(self) -> None:
-        while True:
-            await asyncio.sleep(10)
-            for k, v in sorted(self.counters.items()):
-                print(f"{k}: {v}")
-
-
-def stringify_params(params: dict[str, str]) -> str:
-    return ",".join(f"{k}={v}" for k, v in sorted(params.items()))
