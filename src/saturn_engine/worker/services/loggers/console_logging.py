0be4e49ed763be2e85876d370b5ce7dba990d6b4
isra17 <isra017@gmail.com>
2022-01-19 15:52:12 -0500

    saturn(logging): Add colored logs with structlog

--- src/saturn_engine/worker/services/loggers/console_logging.py
+++ src/saturn_engine/worker/services/loggers/console_logging.py
@@ -1,3 +1,6 @@
+from typing import Any
+from typing import Optional
+
 import logging
 import logging.config
 
@@ -10,20 +13,46 @@ class ConsoleLogging(MinimalService):
     name = "console_logging"
 
     async def open(self) -> None:
-        # if not setup_structlog():
-        setup_logging()
+        if not setup_structlog():
+            setup_logging()
 
 
-# def setup_structlog() -> bool:
-#     try:
-#         import structlog
-#     except ImportError:
-#         logging.warning(
-#             "Console logging setup without structlog. For a more pleasant and "
-#             "colorful experience, install saturn with console (pip install "
-#             "saturn-engine[console])"
-#         )
-#         return False
+def setup_structlog() -> bool:
+    try:
+        import structlog
+    except ImportError:
+        logging.warning(
+            "Console logging setup without structlog. For a more pleasant and "
+            "colorful experience, install structlog (pip install structlog)"
+        )
+        return False
+
+    def unwrap_extra_data(
+        logger: logging.Logger, name: str, event_dict: structlog.types.EventDict
+    ) -> structlog.types.EventDict:
+        record: Optional[logging.LogRecord] = event_dict.get("_record")
+        if record is not None:
+            for k, v in record.__dict__.get("data", {}).items():
+                event_dict[k] = v
+        return event_dict
+
+    pre_chain = [
+        structlog.stdlib.add_log_level,
+        unwrap_extra_data,
+        structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S"),
+    ]
+
+    setup_logging(
+        {
+            "()": structlog.stdlib.ProcessorFormatter,
+            "processors": [
+                structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+                structlog.dev.ConsoleRenderer(),
+            ],
+            "foreign_pre_chain": pre_chain,
+        }
+    )
+    return True
 
 
 class ExtraFormatter(logging.Formatter):
@@ -61,13 +90,14 @@ class ExtraFormatter(logging.Formatter):
         return string
 
 
-def setup_logging() -> None:
+def setup_logging(formatter: Optional[dict[str, Any]] = None) -> None:
     logging.config.dictConfig(
         {
             "version": 1,
             "disable_existing_loggers": True,
             "formatters": {
-                "standard": {
+                "standard": formatter
+                or {
                     "class": __name__ + "." + ExtraFormatter.__name__,
                     "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
                 },
