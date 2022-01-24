56fc58bfb046b30cab56604defd8f0fc0cb26a12
aviau <alexandre@alexandreviau.net>
2022-01-24 12:52:30 -0500

    tester: show an inv sample

--- src/saturn_engine/utils/tester/inventory_test.py
+++ src/saturn_engine/utils/tester/inventory_test.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import asyncio
 
 from saturn_engine.config import default_config_with_env
@@ -10,37 +12,47 @@ from .config.inventory_test import InventoryTest
 from .diff import get_diff
 
 
-def run_saturn_inventory_test(
+def run_saturn_inventory(
     *,
     static_definitions: StaticDefinitions,
-    inventory_test: InventoryTest,
-) -> None:
-
-    inventory_item = static_definitions.inventories[
-        inventory_test.spec.selector.inventory
-    ]
-
+    inventory_name: str,
+    limit: Optional[int] = None,
+    after: Optional[str] = None,
+) -> list[dict]:
+    inventory_item = static_definitions.inventories[inventory_name]
     inventory = work_factory.build_inventory(
         inventory_item=inventory_item,
         services=ServicesManager(
             config=default_config_with_env(),
         ).services,
     )
-
     items: list[dict] = []
 
     async def run_inventory() -> None:
-        limit = inventory_test.spec.limit
         count = 0
-        async for item in inventory.iterate(
-            after=inventory_test.spec.after,
-        ):
+        async for item in inventory.iterate(after=after):
             items.append(asdict(item))
             count = count + 1
             if limit and count >= limit:
                 break
 
     asyncio.run(run_inventory())
+
+    return items
+
+
+def run_saturn_inventory_test(
+    *,
+    static_definitions: StaticDefinitions,
+    inventory_test: InventoryTest,
+) -> None:
+
+    items: list[dict] = run_saturn_inventory(
+        static_definitions=static_definitions,
+        inventory_name=inventory_test.spec.selector.inventory,
+        limit=inventory_test.spec.limit,
+        after=inventory_test.spec.after,
+    )
 
     expected_items: list[dict] = [asdict(item) for item in inventory_test.spec.items]
 
