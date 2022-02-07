3bd09e74a1ad2174e73810b3df7194cd1540ce37
aviau <alexandre@alexandreviau.net>
2022-02-07 09:12:40 -0500

    export client.SaturnClient

---
+++ src/saturn_engine/client/__init__.py
@@ -0,0 +1,9 @@
+from .saturn import SaturnClient
+from .saturn import SyncSaturnClient
+from .worker_manager import WorkerManagerClient
+
+__all__ = [
+    "SaturnClient",
+    "SyncSaturnClient",
+    "WorkerManagerClient",
+]
