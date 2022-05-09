c265089d76216325118de069f1e3fafc8ab7499c
aviau <alexandre@alexandreviau.net>
2022-05-09 09:36:28 -0400

    v0.2.0dev62

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="saturn_engine",
-    version="0.2.0dev61", # Also in `pyproject.toml`
+    version="0.2.0dev62", # Also in `pyproject.toml`
     packages=["saturn_engine"],
     package_dir={"":"src"},
     install_requires=[],
