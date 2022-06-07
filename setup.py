09ea44e236b3298c3ae537165df9a440e1252f17
isra17 <isra017@gmail.com>
2022-06-07 11:19:06 -0400

    tools: setup new read pyproject.toml for version and add script to bump version

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import find_packages
 from setuptools import setup
+import os.path
+import re
+
+def get_version():
+    with open(os.path.join(os.path.dirname(__file__), "pyproject.toml"), "r") as f:
+        for l in f:
+            if m := re.search(r'^version *= *"([^"]+)" *$', l):
+                return m.group(1)
+
 
 setup(
     name="saturn_engine",
-    version="0.2.0dev74", # Also in `pyproject.toml`
+    version=get_version(),
     packages=["saturn_engine"],
     package_dir={"":"src"},
     install_requires=[],
