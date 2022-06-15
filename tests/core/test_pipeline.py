5a388c7530c6e2a64a7174e756a59e8d5b2c0fdc
aviau <alexandre@alexandreviau.net>
2022-06-15 14:26:38 -0400

    instancify_args: load dataclass lists

--- tests/core/test_pipeline.py
+++ tests/core/test_pipeline.py
@@ -21,3 +21,31 @@ def test_instancify_args_optional() -> None:
     )
 
     assert args == {"a": A(field="value")}
+
+
+def test_instancify_args_list() -> None:
+    @dataclasses.dataclass
+    class Carrot:
+        color: str
+
+    def pipeline(carrots: list[Carrot]) -> None:
+        pass
+
+    args: dict[str, object] = {
+        "carrots": [
+            {"color": "orange"},
+            {"color": "purple"},
+        ]
+    }
+
+    PipelineInfo.instancify_args(
+        args=args,
+        pipeline=pipeline,
+    )
+
+    assert args == {
+        "carrots": [
+            Carrot(color="orange"),
+            Carrot(color="purple"),
+        ]
+    }
