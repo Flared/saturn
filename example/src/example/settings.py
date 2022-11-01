d0415faecbefa7120538d8353fa128ae515fb3c7
isra17 <isra017@gmail.com>
2022-11-01 15:42:40 -0400

    Setup example with tracer

--- example/src/example/settings.py
+++ example/src/example/settings.py
@@ -1,15 +1,16 @@
 import os
 
 from saturn_engine.config import SaturnConfig
-from saturn_engine.config import WorkerConfig
+from saturn_engine.config import ServicesManagerConfig
 from saturn_engine.default_config import config as default_config
 from saturn_engine.worker.services.extras.sentry import Sentry
 
 
 class config(SaturnConfig):
-    class worker(WorkerConfig):
+    class services_manager(ServicesManagerConfig):
         services = default_config.services_manager.services + [
-            "saturn_engine.worker.services.extras.sentry.Sentry",
+            "saturn_engine.worker.services.tracing.TracerConfig",
+            "saturn_engine.worker.services.loggers.ConsoleLogging",
         ]
 
     class sentry(Sentry.Options):
