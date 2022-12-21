288567013927a85a0ea9c2c30deebf84e781eee7
Alexandre Viau <alexandre@alexandreviau.net>
2022-12-20 20:43:25 -0500

    config: allow specifying db connection creator (#256)

--- src/saturn_engine/config_definitions.py
+++ src/saturn_engine/config_definitions.py
@@ -20,7 +20,7 @@ class WorkerManagerConfig:
     flask_host: str
     flask_port: int
     database_url: str
-    async_database_url: str
+    database_connection_creator: t.Optional[str]
     static_definitions_directories: list[str]
     static_definitions_jobs_selector: t.Optional[str]
     work_items_per_worker: int
