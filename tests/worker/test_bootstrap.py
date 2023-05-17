5f878d3fe7cddbd204c2fa3dba4b4277b4fe8474
aviau <alexandre@alexandreviau.net>
2023-05-17 16:24:58 -0400

    traceback_data: support notes

--- tests/worker/test_bootstrap.py
+++ tests/worker/test_bootstrap.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 import pickle  # noqa: S403
+import sys
 
 from saturn_engine.worker.executors.bootstrap import RemoteException
 from saturn_engine.worker.executors.bootstrap import wrap_remote_exception
@@ -14,9 +15,12 @@ def raises(
     e: Exception,
     *,
     cause: Optional[Exception] = None,
-    context: Optional[Exception] = None
+    context: Optional[Exception] = None,
+    notes: Optional[list[str]] = None,
 ) -> None:
     x = {"foo": "bar"}  # noqa: F841
+    for n in notes or []:
+        e.add_note(n)
     try:
         if context:
             raise context
@@ -28,12 +32,18 @@ def raises_remote(
     e: Exception,
     *,
     cause: Optional[Exception] = None,
-    context: Optional[Exception] = None
+    context: Optional[Exception] = None,
+    notes: Optional[list[str]] = None,
 ) -> None:
     try:
         # That would be in a remote process
         with wrap_remote_exception():
-            raises(e, cause=cause, context=context)
+            raises(
+                e,
+                cause=cause,
+                context=context,
+                notes=notes,
+            )
     except Exception as e:
         remote_error = pickle.dumps(e)
     else:
@@ -55,6 +65,19 @@ def test_remote_exception() -> None:
         assert e.remote_traceback.exc_type == "ValueError"
         assert e.remote_traceback.stack[-1].line.strip() == "raise e from cause"
         assert e.remote_traceback.stack[-1].locals["x"] == "{'foo': 'bar'}"
+
+    if (sys.version_info[0], sys.version_info[1]) >= (3, 11):
+        try:
+            raises_remote(ValueError("test", 1), notes=["hello I am a note"])
+            raise AssertionError
+        except RemoteException as e:
+            assert isinstance(e, RemoteException)
+            assert e.remote_traceback.__cause__ is None
+            assert e.remote_traceback.__context__ is None
+            assert e.remote_traceback.exc_type == "ValueError"
+            assert e.remote_traceback.stack[-1].line.strip() == "raise e from cause"
+            assert e.remote_traceback.stack[-1].locals["x"] == "{'foo': 'bar'}"
+            assert "hello I am a note" in e.__str__()
 
     try:
         raises_remote(MyError("test", 1), cause=ValueError("cause"))
