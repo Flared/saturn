4195378604dc853ae2856f13545addad2ce8b455
isra17 <isra017@gmail.com>
2023-07-04 18:25:34 -0400

    Replace JobSstore with JobState

--- src/saturn_engine/worker/job.py
+++ src/saturn_engine/worker/job.py
@@ -1,59 +1,47 @@
-from typing import Optional
-
-import abc
 from collections.abc import AsyncGenerator
 
 import asyncstdlib as alib
 
-from saturn_engine.core import Cursor
 from saturn_engine.core import MessageId
 from saturn_engine.core import TopicMessage
+from saturn_engine.core.api import QueueItemWithState
 from saturn_engine.utils.log import getLogger
-from saturn_engine.utils.options import OptionsSchema
+from saturn_engine.worker.services import Services
+from saturn_engine.worker.services.job_state.service import JobStateService
 
-from ..inventories import Inventory
-from ..topics import Topic
-from ..topics import TopicOutput
-
-
-class JobStore(OptionsSchema, abc.ABC):
-    @abc.abstractmethod
-    async def load_cursor(self) -> Optional[Cursor]:
-        pass
-
-    @abc.abstractmethod
-    async def save_cursor(self, *, after: Cursor) -> None:
-        pass
-
-    @abc.abstractmethod
-    async def set_completed(self) -> None:
-        pass
-
-    @abc.abstractmethod
-    async def set_failed(self, error: Exception) -> None:
-        pass
+from .inventories import Inventory
+from .topics import Topic
+from .topics import TopicOutput
 
 
 class Job(Topic):
-    def __init__(self, *, inventory: Inventory, store: JobStore) -> None:
+    def __init__(
+        self,
+        *,
+        inventory: Inventory,
+        queue_item: QueueItemWithState,
+        services: Services
+    ) -> None:
         self.logger = getLogger(__name__, self)
         self.inventory = inventory
-        self.store = store
+        self.services = services
         self.batch_size = 10
+        self.queue_item = queue_item
+        self.state_service = services.cast_service(JobStateService)
 
     async def run(self) -> AsyncGenerator[TopicOutput, None]:
         try:
-            after = await self.store.load_cursor()
+            cursor = self.queue_item.state.cursor
             done = None
 
             async with alib.scoped_iter(
-                self.inventory.iterate(after=after)
+                self.inventory.iterate(after=cursor)
             ) as iterator:
                 while done is not True:
                     try:
                         done = True
                         async for item in alib.islice(iterator, self.batch_size):
-                            after = item.cursor
+                            cursor = item.cursor
                             done = False
                             message = TopicMessage(
                                 id=MessageId(item.id),
@@ -63,10 +51,12 @@ class Job(Topic):
                             )
                             yield message
                     finally:
-                        if not done and after is not None:
-                            await self.store.save_cursor(after=after)
+                        if not done and cursor is not None:
+                            self.state_service.set_job_cursor(
+                                self.queue_item.name, cursor=cursor
+                            )
 
-            await self.store.set_completed()
+            self.state_service.set_job_completed(self.queue_item.name)
         except Exception as e:
             self.logger.exception("Exception raised from job")
-            await self.store.set_failed(e)
+            self.state_service.set_job_failed(self.queue_item.name, error=e)
