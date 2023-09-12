c57d0a0944f6814769a52a3e6ec15e1e538c0dda
isra17 <isra017@gmail.com>
2023-09-12 15:57:04 -0400

    Fix batch inventory to manage cursor and items context

--- tests/worker/inventories/test_batching_inventory.py
+++ tests/worker/inventories/test_batching_inventory.py
@@ -1,3 +1,5 @@
+import json
+
 import asyncstdlib as alib
 import pytest
 
@@ -18,7 +20,7 @@ async def test_batching_inventory() -> None:
         },
         services=None,
     )
-    items = await alib.list(inventory.iterate())
+    items = await alib.list(inventory.run())
 
     assert [(i.id, i.args) for i in items] == [
         ("2", {"batch": [{"a": "0"}, {"a": "1"}, {"a": "2"}]}),
@@ -34,7 +36,19 @@ async def test_batching_inventory() -> None:
         ),
     ]
 
-    items = await alib.list(inventory.iterate(after=Cursor("4")))
+    async with items[1]:
+        pass
+
+    assert (c := inventory.cursor)
+    assert json.loads(c) == {"v": 1, "p": ["3", "4", "5"]}
+
+    async with items[0]:
+        pass
+
+    assert (c := inventory.cursor)
+    assert json.loads(c) == {"v": 1, "a": "5"}
+
+    items = await alib.list(inventory.run(after=Cursor("4")))
 
     assert [(i.id, i.args) for i in items] == [
         ("7", {"batch": [{"a": "5"}, {"a": "6"}, {"a": "7"}]}),
