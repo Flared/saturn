5b62875123860ecfecbdfff3b267e0195bc8b28e
isra17 <isra017@gmail.com>
2023-09-12 15:57:04 -0400

    Fix batch inventory to manage cursor and items context

--- src/saturn_engine/worker/inventories/batching.py
+++ src/saturn_engine/worker/inventories/batching.py
@@ -1,19 +1,18 @@
-from typing import AsyncIterator
-from typing import Optional
+import typing as t
 
 import dataclasses
+from contextlib import AsyncExitStack
 
 import asyncstdlib as alib
 
 from saturn_engine.core import Cursor
 from saturn_engine.core.api import ComponentDefinition
+from saturn_engine.worker.inventory import Item
+from saturn_engine.worker.inventory import IteratorInventory
 from saturn_engine.worker.services import Services
 
-from . import Inventory
-from . import Item
 
-
-class BatchingInventory(Inventory):
+class BatchingInventory(IteratorInventory):
     @dataclasses.dataclass
     class Options:
         inventory: ComponentDefinition
@@ -22,29 +21,38 @@ class BatchingInventory(Inventory):
     def __init__(
         self, *, options: Options, services: Services, **kwargs: object
     ) -> None:
-        self.batch_size = options.batch_size
+        super().__init__(batch_size=options.batch_size)
 
         # This import must be done late since work_factory depends on this module.
         from saturn_engine.worker.work_factory import build_inventory
 
         self.inventory = build_inventory(options.inventory, services=services)
 
-    async def next_batch(self, after: Optional[Cursor] = None) -> list[Item]:
+    async def _next_batch(self) -> list[Item]:
         batch: list[Item] = await alib.list(
-            alib.islice(self.inventory.iterate(after=after), self.batch_size)
+            alib.islice(self._run_iter, self.batch_size)
         )
         return batch
 
-    async def iterate(self, after: Optional[Cursor] = None) -> AsyncIterator[Item]:
-        while True:
-            batch = await self.next_batch(after)
-            if not batch:
-                return
+    async def iterate(self, after: t.Optional[Cursor] = None) -> t.AsyncIterator[Item]:
+        async with alib.scoped_iter(self.inventory.run(after=after)) as run_iter:
+            self._run_iter = run_iter
 
-            last_item = batch[-1]
-            yield Item(
-                id=last_item.id,
-                cursor=last_item.cursor,
-                args={"batch": [item.args for item in batch]},
-            )
-            after = last_item.cursor
+            while True:
+                batch = await self._next_batch()
+                if not batch:
+                    return
+
+                last_item = batch[-1]
+                async with AsyncExitStack() as stack:
+                    for item in batch:
+                        await stack.enter_async_context(item)
+                    yield dataclasses.replace(
+                        last_item,
+                        args={"batch": [item.args for item in batch]},
+                        context=stack.pop_all(),
+                    )
+
+    @property
+    def cursor(self) -> t.Optional[Cursor]:
+        return self.inventory.cursor
