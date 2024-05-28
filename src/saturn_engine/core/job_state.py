486713461ae5ece9961ca5194b76509c69a1107d
isra17 <isra017@gmail.com>
2024-05-28 15:07:21 -0400

    service(State): Allow to override the updated state cursor

--- src/saturn_engine/core/job_state.py
+++ src/saturn_engine/core/job_state.py
@@ -1,11 +1,13 @@
 import dataclasses
 
+from saturn_engine.core import Cursor
 from saturn_engine.core.pipeline import PipelineEvent
 
 
 @dataclasses.dataclass
 class CursorStateUpdated(PipelineEvent):
     state: dict
+    cursor: Cursor | None = None
 
 
 class CursorState:
