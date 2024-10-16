bbf8c90dcc60c109564ae9ab041516e703fc2c8e
Olivier Michaud <olivier.michaud@flare.systems>
2024-10-16 15:14:37 -0400

    Create a factory to load static definition, separate the creation flow

--- src/saturn_engine/worker_manager/context.py
+++ src/saturn_engine/worker_manager/context.py
@@ -1,6 +1,5 @@
-from functools import cached_property
-
 from saturn_engine.config import WorkerManagerConfig
+from saturn_engine.utils.sqlalchemy import AnySession
 from saturn_engine.worker_manager.config.declarative import filter_with_jobs_selector
 from saturn_engine.worker_manager.config.declarative import load_definitions_from_paths
 
@@ -15,22 +14,35 @@ class WorkerManagerContext:
 
     def __init__(self, config: WorkerManagerConfig) -> None:
         self.config: WorkerManagerConfig = config
+        self._static_definitions: StaticDefinitions | None
 
-    @cached_property
+    @property
     def static_definitions(self) -> StaticDefinitions:
-        """
-        Static definitions contain objects defined in a declarative configuration:
-        - Inventories
-        - Topics
-        - Jobs
-        - JobDefinitions
-        """
-        definitions = load_definitions_from_paths(
-            self.config.static_definitions_directories
+        assert (
+            self._static_definitions
+        ), "Static definitions need to be set before usage"
+        return self._static_definitions
+
+    def reset_static_definition(self, session: AnySession) -> None:
+        self._static_definitions = _load_static_definition(
+            session=session, config=self.config
         )
-        if self.config.static_definitions_jobs_selector:
-            definitions = filter_with_jobs_selector(
-                definitions=definitions,
-                selector=self.config.static_definitions_jobs_selector,
-            )
-        return definitions
+
+
+def _load_static_definition(
+    config: WorkerManagerConfig, session: AnySession
+) -> StaticDefinitions:
+    """
+    Static definitions contain objects defined in a declarative configuration:
+    - Inventories
+    - Topics
+    - Jobs
+    - JobDefinitions
+    """
+    definitions = load_definitions_from_paths(config.static_definitions_directories)
+    if config.static_definitions_jobs_selector:
+        definitions = filter_with_jobs_selector(
+            definitions=definitions,
+            selector=config.static_definitions_jobs_selector,
+        )
+    return definitions
